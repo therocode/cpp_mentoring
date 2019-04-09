40d39a0930cc431fa6a807d505bd0cb53f84806c
NotAPenguin <michiel.vandeginste@gmail.com>
2019-04-09 12:29:22 +0200

    Add files via upload

---
+++ 1_using_filestreams.cpp
@@ -0,0 +1,19 @@
+/*
+1. How to use filestreams in your project
+
+To use C++ filestreams in your project, all you have to do is include the right header: <fstream>
+*/
+
+#include <fstream>
+
+/*
+This header contains a few classes, most importantly std::fstream, std::ifstream and std::ofstream.
+It's not too hard to guess where these names come from:
+std::fstream - FileSTREAM
+std::ifstream - InputFileSTREAM
+std::ofstream - OutputFileSTREAM
+
+As the names of the classes say, std::ifstream is used to read input from a file, std::ofstream is used to
+write data to a file. std::fstream inherits from both of these and is used for both.
+this class yourself.
+*/
