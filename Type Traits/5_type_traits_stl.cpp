61fcfebc05d42f0c511e5716d7509efe5e5a837e
NotAPenguin <michiel.vandeginste@gmail.com>
2019-04-20 16:37:35 +0200

    Add files via upload

---
+++ Type Traits/5_type_traits_stl.cpp
@@ -0,0 +1,12 @@
+/*
+
+5. Type traits and the STL
+
+*/
+
+/*
+The STL provides many type traits for you already. It also relies heavily on type traits in the implementation
+of some classes, for example std::string uses a char_traits<CharT> class as mentioned earlier. For a full
+list of STL type traits, see
+http://cppreference.com/reference/en/cpp/types
+*/
---
+++ Type Traits/6_conclusion.cpp
@@ -0,0 +1,17 @@
+/*
+
+6. Conclusion and assignment
+
+*/
+
+/*
+As we have seen, type traits are a very powerful tool when it comes to generic programming. For this assignment,
+you will have to create a few type traits like the ones in the STL. 
+
+Type traits you have to implement are:
+std::is_array<T>
+std::is_const<T>
+
+A few more challenging ones to try are std::conditional<bool, T, U> or std::common_type<T, U>.
+
+*/
